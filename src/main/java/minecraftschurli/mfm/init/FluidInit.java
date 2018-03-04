eb03c43b23db4253ddbf695011e3ff523fb3d79b
Minecraftschurli <minecraftschurli@gmail.com>
2018-03-04 22:18:25 +0100

    .

--- src/main/java/minecraftschurli/mfm/init/FluidInit.java
+++ src/main/java/minecraftschurli/mfm/init/FluidInit.java
@@ -13,9 +13,9 @@ public class FluidInit
     public static final List<Fluid> FLUIDS = new List<>();
 
 
-    public static final Fluid FLUID_VULCANIUM = new FluidMaterial("fluid_vulcanium", new Color(255,110,0,255));
+    public static final Fluid FLUID_VULCANIUM = new FluidMaterial("fluid_vulcanium", new Color(255,110,0,255)).setTemperature(100000);
     public static final Fluid FLUID_KANDIUM = new FluidMaterial("fluid_kandium",new Color(0,228,199,255));
-    public static final Fluid FLUID_KANADIUM = new FluidMaterial("fluid_kanadium",new Color(0,119,255,255)).setTemperature(100000);
+    public static final Fluid FLUID_KANADIUM = new FluidMaterial("fluid_kanadium",new Color(0,119,255,255));
     public static final Fluid FLUID_STANIUM = new FluidMaterial("fluid_stanium",new Color(255,40,0,255)).setTemperature(1000000);
 
 }
