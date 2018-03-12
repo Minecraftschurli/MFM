60754cda89088fbbf12ac0d1a0f8f9db0c17ef10
Minecraftschurli <minecraftschurli@gmail.com>
2018-03-12 19:21:44 +0100

    .

--- src/main/java/minecraftschurli/mfm/objects/blocks/machines/alloyer/GuiAlloyingFurnace.java
+++ src/main/java/minecraftschurli/mfm/objects/blocks/machines/alloyer/GuiAlloyingFurnace.java
@@ -17,6 +17,13 @@ public class GuiAlloyingFurnace extends GuiContainer {
 		this.player = player;
 		this.tileentity = tileentity;
 	}
+
+	@Override
+	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+		this.drawDefaultBackground();
+		super.drawScreen(mouseX, mouseY, partialTicks);
+		this.renderHoveredToolTip(mouseX, mouseY);
+	}
 	
 	@Override
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
