4db17de32a99af313aa4a9936a01ed9a42a86d9a
Minecraftschurli <minecraftschurli@gmail.com>
2018-03-12 16:58:17 +0100

    .

--- src/main/java/minecraftschurli/mfm/util/Reference.java
+++ src/main/java/minecraftschurli/mfm/util/Reference.java
@@ -9,6 +9,7 @@ public class Reference {
     public static final String MAJOR_API_VERSION = "0";
     public static final String MINOR_API_MOD_VERSION = "1";
     public static final String PATCH_VERSION = "4";
+    public static final String V = "1.12.2" + "-" + "0.0.1.4";
     public static final String VERSION = MCVERSION.replace("[", "").replace("]", "") + "-" + MAJOR_MOD_VERSION + "." + MAJOR_API_VERSION + "." + MINOR_API_MOD_VERSION + "." + PATCH_VERSION;
 	public static final String CLIENTSIDE = "minecraftschurli.mfm.proxy.ClientProxy";
 	public static final String SERVERSIDE = "minecraftschurli.mfm.proxy.ServerProxy";
