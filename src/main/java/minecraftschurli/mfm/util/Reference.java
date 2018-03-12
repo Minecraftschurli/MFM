ab2f6d2503166246b6db7a969b0962e86ff12fa4
Minecraftschurli <minecraftschurli@gmail.com>
2018-03-12 16:38:01 +0100

    .

--- src/main/java/minecraftschurli/mfm/util/Reference.java
+++ src/main/java/minecraftschurli/mfm/util/Reference.java
@@ -5,9 +5,9 @@ public class Reference {
 	public static final String MODID = "mfm";
 	public static final String NAME = "MFM";
 	public static final String MCVERSION = "[1.12.2]";
-    public static final String MAJOR_MOD_VERSION = "1.0";
-    public static final String MAJOR_API_VERSION = "1.0";
-    public static final String MINOR_API_MOD_VERSION = "1.0";
+    public static final String MAJOR_MOD_VERSION = "0";
+    public static final String MAJOR_API_VERSION = "0";
+    public static final String MINOR_API_MOD_VERSION = "1";
     public static final String PATCH_VERSION = "4";
     public static final String VERSION = MCVERSION.replace("[", "").replace("]", "") + "-" + MAJOR_MOD_VERSION + "." + MAJOR_API_VERSION + "." + MINOR_API_MOD_VERSION + "." + PATCH_VERSION;
 	public static final String CLIENTSIDE = "minecraftschurli.mfm.proxy.ClientProxy";
